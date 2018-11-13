361b0c955dcf557d2aa55b261b7d7d394877e2b0
mfgomezb <mgomezb89@gmail.com>
2018-11-13 11:55:37 +0100

    ADDED: ENDPOINT MOD:  HAPROXY.CFG, COMPOSE.YML
    endpoint /version to display the version number and HAPROXY and COMPOSE to include consul, reg and bal

---
+++ message/controllers/sendMessage.js
@@ -0,0 +1,96 @@
+const http = require("http");
+const saveMessage = require("../clients/saveMessage");
+
+const random = n => Math.floor(Math.random() * Math.floor(n));
+
+module.exports = function(message, credit) {
+  const messageContent = message
+  const messageJSON = JSON.stringify(message);
+  
+  let current_credit = credit
+
+    if (current_credit > 0) {
+      
+      const postOptions = {
+        // host: "exercise4_messageapp_1",
+        // host: "localhost",
+        host: "messageapp",
+        port: 3000,
+        path: "/message",
+        method: "post",
+        json: true,
+        headers: {
+          "Content-Type": "application/json",
+          "Content-Length": Buffer.byteLength(messageJSON)
+        }
+      };
+
+      let postReq = http.request(postOptions);
+
+      postReq.on("response", postRes => {
+        if (postRes.statusCode === 200) {
+          // updateMessage
+          saveMessage(
+            {
+              ...messageContent,
+              status: "OK"
+            },
+            function(_result, error) {
+              if (error) {
+                console.log(error)
+              } else {
+                console.log(messageContent)
+              }
+            }
+          );
+        } else {
+          console.error("Error while sending message");
+          // updateMessage
+          saveMessage(
+            {
+              ...messageContent,
+              status: "ERROR"
+            },
+            () => {
+              console.log("Internal server error: SERVICE ERROR")
+            }
+          );
+        }
+      });
+
+      postReq.setTimeout(random(6000));
+
+      postReq.on("timeout", () => {
+        console.error("Timeout Exceeded!");
+        postReq.abort();
+        saveMessage(
+          {
+            ...messageContent,
+            status: "TIMEOUT"
+          },
+          () => {
+            console.log("Internal server error: TIMEOUT")
+          }
+        );
+      });
+
+      postReq.on("error", () => {
+        console.error("Error while sending message");
+        saveMessage(
+          {
+            ...messageContent,
+            status: "ERROR"
+          },
+          () => {
+            console.log("Internal server error: SERVICE ERROR")
+          }
+        );
+      });
+
+      postReq.write(messageJSON);
+      postReq.end();
+      
+    } else {
+      console.log("No credit error")
+    }
+};
---
+++ message/index.js
@@ -0,0 +1,69 @@
+require('dotenv').config();
+
+const http = require("http");
+const express = require("express");
+const bodyParser = require("body-parser");
+const { Validator, ValidationError } = require("express-json-validator-middleware");
+const { queueCreditCheck }  = require("./controllers/queueCredit");
+const service_port = process.env.SERVICE_PORT || 9007
+const getMessages = require("./controllers/getMessages");
+const getHealth = require("./controllers/getHealth");
+const getVersion = require("./controllers/getVersion");
+const getMessageStatus = require("./controllers/getMessageStatus");
+
+require("./controllers/queueTx");
+
+const app = express();
+
+const validator = new Validator({ allErrors: true });
+const { validate } = validator;
+
+const messageSchema = {
+  type: "object",
+  required: ["destination", "body"],
+  properties: {
+    destination: {
+      type: "string"
+    },
+    body: {
+      type: "string"
+    },
+    location: {
+      name: {
+        type: "string"
+      },
+      cost: {
+        type: "number"
+      }
+    }
+  }
+};
+
+app.post(
+  "/messages",
+  bodyParser.json(),
+  validate({ body: messageSchema }),
+  queueCreditCheck
+);
+
+app.get("/messages", getMessages);
+
+app.get("/health", getHealth);
+
+app.get("/version", getVersion);
+
+app.get("/messages/:messageId/status", getMessageStatus);
+
+app.use(function(err, req, res, next) {
+  console.log(res.body);
+  if (err instanceof ValidationError) {
+    res.sendStatus(400);
+  } else {
+    res.sendStatus(500);
+  }
+});
+
+
+app.listen(service_port, function() {
+  console.log(`App started on PORT ${service_port}`);
+});
